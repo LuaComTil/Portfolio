91aa5c939782e809335a386d9c3fd051ed1a103b
LuaComTil <luadefreitasramos@gmail.com>
2024-06-16 16:53:18 -0300

    minor fixes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,6 @@
 import Header from "./Sections/Hero/Header";
 import Hero from "./Sections/Hero/Hero";
 import Portfolio from "./Sections/Portfolio/Portfolio"
-import PortfolioFilter from "./Sections/Portfolio/PortfolioFilter";
 //import SwipeUpIcon from '@mui/icons-material/SwipeUp';
 //import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 const Home = () => {
